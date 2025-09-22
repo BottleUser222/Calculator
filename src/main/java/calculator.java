247f7a9a23757d74610b41010d445dccad490ae0
BottleUser222 <marko.s.mirkovic@gmail.com>
2025-09-22 11:25:40 +0200

    Added java.util.Scanner along with the code that takes 2 numbers given by the user and returns a sum.

--- src/main/java/calculator.java
+++ src/main/java/calculator.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
 public class calculator {
         public static void main(String[] args){
-            int num1 = 10;
-            int num2 = 20;
+            Scanner input = new Scanner(System.in);
+            System.out.print("Please enter a number: ");
+            while (!input.hasNextInt()){
+                System.out.println("That's not a number! Input a number: ");
+                input.next();
+            }
+            int num1 = input.nextInt();
+            System.out.print("Please enter another number: ");
+            while (!input.hasNextInt()){
+                System.out.println("That's not a number! Input a number: ");
+                input.next();
+            }
+            int num2 = input.nextInt();
             int sum = num1 + num2;
             System.out.println("The sum is " + sum);
         }
