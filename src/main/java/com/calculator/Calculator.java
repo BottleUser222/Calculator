60e9736e572f2d38b312fe5cfd2eb7735130ff1e
BottleUser222 <marko.s.mirkovic@gmail.com>
2025-09-24 20:09:37 +0200

    Add a Calculator constructor and an add() method which adds two integers, given by the user, inside the Calculator class.

--- src/main/java/com/calculator/Calculator.java
+++ src/main/java/com/calculator/Calculator.java
@@ -1,4 +1,13 @@
 package com.calculator;
 
 public class Calculator {
+    int x;
+    int y;
+    Calculator(int x,int y){
+        this.x=x;
+        this.y=y;
+    }
+    void add(){
+        System.out.println("The sum is " + (this.x+this.y));
+    }
 }
--- src/main/java/com/calculator/Main.java
+++ src/main/java/com/calculator/Main.java
@@ -1,6 +1,9 @@
 package com.calculator;
 
 import java.util.Scanner;
+
+import static javax.management.Query.plus;
+
 public class Main {
         public static void main(String[] args){
             Scanner input = new Scanner(System.in);
@@ -16,12 +19,8 @@ public class Main {
                 input.next();
             }
             int num2 = input.nextInt();
-            int sum = num1 + num2;
-            if (num1==2147483647 || num2==2147483647){
-                System.out.println("One of the numbers you entered is too large!");
-            }
-            else {
-                System.out.println("The sum is " + sum);
-            }
+            Calculator calc = new Calculator(num1,num2);
+            if (num1==2147483647 || num2==2147483647) System.out.println("One of the numbers you entered is too large!");
+            else calc.add();
         }
     }
