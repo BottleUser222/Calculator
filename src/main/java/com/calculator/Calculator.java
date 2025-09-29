27a9f6cb030381eb54a5f5a36b08aec0e094bd82
BottleUser222 <marko.s.mirkovic@gmail.com>
2025-09-29 20:12:49 +0200

    Add subtraction, multiplication and division methods to the Calculator class. Move the if statement which checks for a possible overflow from Main to Calculator class and create different if statements to each method. Add a new question in Main which asks the user what operation they want to use. Add 4 statements to check for the operation the user chose and use the correct operation.

--- src/main/java/com/calculator/Calculator.java
+++ src/main/java/com/calculator/Calculator.java
@@ -1,13 +1,36 @@
 package com.calculator;
 
 public class Calculator {
-    private int frst;
-    private int scnd;
+    private int firstNumber;
+    private int secondNumber;
     public Calculator(int x,int y){
-        this.frst=x;
-        this.scnd=y;
+        this.firstNumber=x;
+        this.secondNumber=y;
     }
     public void add(){
-        System.out.println("The sum is " + (this.frst+this.scnd));
+        if ((firstNumber>0 && secondNumber>0 && firstNumber+secondNumber<0) ||
+        (firstNumber<0 && secondNumber<0 && firstNumber+secondNumber>0))
+        System.out.println("The sum of the given numbers reached an overflow!");
+
+        else System.out.println("The sum is " + (this.firstNumber+this.secondNumber));
     }
+    public void subtract(){
+        if ((firstNumber>0 && secondNumber<0 && firstNumber-secondNumber<0) ||
+        (firstNumber<0 && secondNumber>0 && firstNumber-secondNumber>0))
+        System.out.println("The difference of the given numbers reached an overflow!");
+
+        else System.out.println("The difference is " + (this.firstNumber-this.secondNumber));
+    }
+    public void multiply(){
+        if (Math.abs(firstNumber*secondNumber)<firstNumber || Math.abs(firstNumber*secondNumber)<secondNumber)
+        System.out.println("The product of the given numbers reached an overflow!");
+
+        else System.out.println("The product is " + (this.firstNumber*this.secondNumber));
+    }
+    public void divide(){
+        if (secondNumber==0) System.out.println("You cannot divide by zero!");
+
+        else System.out.println("The quotient is " + (firstNumber/secondNumber));
+    }
+
 }
--- src/main/java/com/calculator/Main.java
+++ src/main/java/com/calculator/Main.java
@@ -5,20 +5,30 @@ import java.util.Scanner;
 public class Main {
         public static void main(String[] args){
             Scanner input = new Scanner(System.in);
+            String operands = "+-*/";
+            System.out.print("Please choose a basic operation ( +, -, *, / ): ");
+            char operation = input.next().charAt(0);
+            while (!operands.contains(String.valueOf(operation))) {
+                System.out.print("Please choose a symbol as shown in the brackets ( +, -, *, / ): ");
+                operation = input.next().charAt(0);
+            }
             System.out.print("Please enter a number: ");
             while (!input.hasNextInt()){
-                System.out.println("That's not a number! Input a number: ");
                 input.next();
+                System.out.print("That's not a number! Input a number: ");
             }
             int num1 = input.nextInt();
             System.out.print("Please enter another number: ");
             while (!input.hasNextInt()){
-                System.out.println("That's not a number! Input a number: ");
                 input.next();
+                System.out.print("That's not a number! Input a number: ");
             }
             int num2 = input.nextInt();
             Calculator calc = new Calculator(num1,num2);
-            if ((num1>0 && num2>0 && num1+num2<0) || (num1<0 && num2<0 && num1+num2>0)) System.out.println("The sum of the given numbers reached an overflow!");
-            else calc.add();
+
+            if (operation=='+') calc.add();
+            if (operation=='-') calc.subtract();
+            if (operation=='*') calc.multiply();
+            else calc.divide();
         }
     }
