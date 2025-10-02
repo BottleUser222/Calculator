8c78f182d333eca7694b0eaa06515d0ba33ec5a5
BottleUser222 <marko.s.mirkovic@gmail.com>
2025-10-02 18:54:32 +0200

    Add 3 methods in Main class that resolve both numbers the user provides and the operation that the user wants to use.

--- src/main/java/com/calculator/Main.java
+++ src/main/java/com/calculator/Main.java
@@ -5,30 +5,41 @@ import java.util.Scanner;
 public class Main {
         public static void main(String[] args){
             Scanner input = new Scanner(System.in);
-            String operands = "+-*/";
-            System.out.print("Please choose a basic operation ( +, -, *, / ): ");
-            char operation = input.next().charAt(0);
-            while (!operands.contains(String.valueOf(operation))) {
-                System.out.print("Please choose a symbol as shown in the brackets ( +, -, *, / ): ");
-                operation = input.next().charAt(0);
-            }
-            System.out.print("Please enter a number: ");
-            while (!input.hasNextInt()){
-                input.next();
-                System.out.print("That's not a number! Input a number: ");
-            }
-            int num1 = input.nextInt();
-            System.out.print("Please enter another number: ");
-            while (!input.hasNextInt()){
-                input.next();
-                System.out.print("That's not a number! Input a number: ");
-            }
-            int num2 = input.nextInt();
+            char operation = resolveOperation(input);
+            Calculator calc = new Calculator(resolveFirstNumber(input),resolveSecondNumber(input));
 
-            Calculator calc = new Calculator(num1,num2);
             if (operation=='+') { calc.add(); }
             if (operation=='-') { calc.subtract(); }
             if (operation=='*') { calc.multiply(); }
             if (operation=='/') { calc.divide(); }
+        }
+
+
+
+        private static char resolveOperation(Scanner s){
+            String operands = "+-*/";
+            System.out.print("Please choose a basic operation ( +, -, *, / ): ");
+            char operation = s.next().charAt(0);
+            while (!operands.contains(String.valueOf(operation))) {
+                System.out.print("Please choose a symbol as shown in the brackets ( +, -, *, / ): ");
+                operation = s.next().charAt(0);
+            }
+            return operation;
+        }
+        private static int resolveFirstNumber(Scanner s){
+            System.out.print("Please enter a number: ");
+            while (!s.hasNextInt()){
+                s.next();
+                System.out.print("That's not a number! Input a number: ");
+            }
+            return s.nextInt();
+        }
+        private static int resolveSecondNumber(Scanner s){
+            System.out.print("Please enter another number: ");
+            while (!s.hasNextInt()){
+                s.next();
+                System.out.print("That's not a number! Input a number: ");
+            }
+            return s.nextInt();
         }
     }
